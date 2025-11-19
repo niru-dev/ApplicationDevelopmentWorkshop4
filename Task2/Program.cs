bd526402e08f055b86b3ab87b8a71be338ed5fdf
Niru200374 <nirutasilwal21@gmail.com>
2025-11-19 11:58:21 +0545

    Task 2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.PrintWelcome();
+            int sum = Calculator.Add(5, 5);
+            Console.WriteLine($"Sum: {sum}");
+            int product = Calculator.Multiply(7);
+            Console.WriteLine($"Product: {product}");
+
+        }
+    }
+}
---
+++ Task2/calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public static class Calculator
+    {
+        public static void PrintWelcome()
+        {
+            Console.WriteLine("Welcome to the Calculator!");
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+
+        }
+
+        public static int Multiply(int num1, int num2 = 1)
+        {
+            int result = num1 * num2;
+            return result;
+        }
+
+
+    }
+}
