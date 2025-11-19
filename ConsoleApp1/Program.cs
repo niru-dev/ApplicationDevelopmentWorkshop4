c99e786e9ee7edd39d3c41e9f70c9b633afd6e11
Niru200374 <nirutasilwal21@gmail.com>
2025-11-19 11:48:27 +0545

    Task 1 done

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student stud1 = new student();
+            student stud2 = new student();
+            stud1.id = 1;
+            stud1.name ="Neeruu";
+            stud1.age = 21;
+
+            stud2.id = 2;
+            stud2.name ="Nishu";
+            stud2.age = 15;
+            student.college= "Itahari International";
+
+            Console.WriteLine("student 1:");
+            Console.WriteLine($"ID:{stud1.name}, Age: {stud1.age} college: {student.college}");
+         
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class student
+    {
+        public int id;
+        public string name;
+        public int age;
+
+
+        public static string college;
+    }
+}
